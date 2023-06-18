36ab5c2f6120252515aca62d9b1a738edc5a8b73
Richard Walker <digitalsadhu@gmail.com>
2023-06-19 10:46:34 +1200

    fix: add dsd polyfill for SSR

--- server.js
+++ server.js
@@ -6,19 +6,34 @@ import React from "react";
  * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the client.
  * @param {React.FunctionComponent | React.ComponentClass | string} app - The React component/app to render.
  * @param {React.Attributes | null} props - Application props. These will be serialised and passed to the client for hydration as well as used to SSR the application/component.
+ * @param {string} shadowRootMode - The shadow root mode to use. Defaults to "open".
  * @returns {string}
  */
-export function ssr(name, app, props) {
-  return `
-    <${name}>
-      <template shadowrootmode="open">
-        <div id="${name}">${ReactDOM.renderToString(React.createElement(app, props))}</div>
-      </template>
-      <script>
-        window.__REACT_DATA__ = window.__REACT_DATA__ || new Map();
-        window.__REACT_DATA__.set('${name}', ${JSON.stringify(props)});
-      </script>
-    </${name}>`;
+export function ssr(name, app, props, shadowRootMode = "open") {
+  return `<script>
+  window.__REACT_DATA__ = window.__REACT_DATA__ || new Map();
+  window.__REACT_DATA__.set('${name}', ${JSON.stringify(props)});
+</script>
+<style>${name}:not(:defined) > template[shadowrootmode] ~ *  { display: none; }</style>
+<${name}>
+  <template shadowrootmode="${shadowRootMode}">
+    <div id="${name}">${ReactDOM.renderToString(React.createElement(app, props))}</div>
+  </template>
+</${name}>
+<script>
+  (function attachShadowRoots(root) {
+    if (!HTMLTemplateElement.prototype.hasOwnProperty('shadowRootMode')) {
+      root.querySelectorAll("template[shadowrootmode]").forEach(template => {
+        const mode = template.getAttribute("shadowrootmode");
+        const shadowRoot = template.parentNode.attachShadow({ mode });
+        shadowRoot.appendChild(template.content);
+        template.remove();
+        attachShadowRoots(shadowRoot);
+      });
+    }
+  })(document.currentScript.previousElementSibling);
+</script>
+    `;
 }
 
 /**
@@ -28,10 +43,9 @@ export function ssr(name, app, props) {
  * @returns {string}
  */
 export function csr(name, props) {
-  return `
-    <script>
-      window.__REACT_DATA__ = window.__REACT_DATA__ || new Map();
-      window.__REACT_DATA__.set('${name}', ${JSON.stringify(props)});
-    </script>
-    <${name}></${name}>`;
+  return `<script>
+  window.__REACT_DATA__ = window.__REACT_DATA__ || new Map();
+  window.__REACT_DATA__.set('${name}', ${JSON.stringify(props)});
+</script>
+<${name}></${name}>`;
 }
