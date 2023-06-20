7c070386952d263be335549fa3f2aaa7175b4532
Richard Walker <digitalsadhu@gmail.com>
2023-06-20 15:40:19 +1200

    chore: update types

--- types/client.d.ts
+++ types/client.d.ts
@@ -1,12 +1,9 @@
 /**
  * Hydrates an SSR'd React app on the client side by name and component. Expects ssr to be have been called on the server.
  * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the server.
- * @param {React.FunctionComponent | React.ComponentClass | string} app - The React component/app to render.
+ * @param {any} app - The React component/app to render.
+ * @param {{ reviver?: any }} [options] - Options object
  */
-export function ssr(name: string, app: React.FunctionComponent | React.ComponentClass | string): void;
-/**
- * Client side renders a React app by name and component. Expects the function csr to have been called on the server.
- * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the server.
- * @param {React.FunctionComponent | React.ComponentClass | string} app - The React component/app to render.
- */
-export function csr(name: string, app: React.FunctionComponent | React.ComponentClass | string): void;
+export function ssr(name: string, app: any, { reviver }?: {
+    reviver?: any;
+}): void;
