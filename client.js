41b17306ca4b3ffe7eb3393603a2463e4d8340ff
Richard Walker <digitalsadhu@gmail.com>
2023-06-20 14:10:44 +1200

    feat: add support for replacer and reviver functions

--- client.js
+++ client.js
@@ -4,18 +4,20 @@ import React from "react";
 /**
  * Hydrates an SSR'd React app on the client side by name and component. Expects ssr to be have been called on the server.
  * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the server.
- * @param {React.FunctionComponent | React.ComponentClass | string} app - The React component/app to render.
+ * @param {any} app - The React component/app to render.
+ * @param {any} [reviver] - A function that transforms data serialized by ssr.
  */
-export function ssr(name, app) {
+export function ssr(name, app, reviver) {
   customElements.define(
     name,
     class extends HTMLElement {
       constructor() {
         super();
         if (this.shadowRoot) {
+          const data = this.shadowRoot.querySelector(`script[type="application/json"]`);
           const el = this.shadowRoot.querySelector(`#${name}`);
           // @ts-ignore
-          const props = window.__REACT_DATA__.get(name);
+          const props = JSON.parse(data.textContent, reviver);
           if (el) {
             hydrateRoot(el, React.createElement(app, props));
           }
@@ -26,7 +28,7 @@ export function ssr(name, app) {
 }
 
 /**
- * Client side renders a React app by name and component. Expects the function csr to have been called on the server.
+ * Client side renders a React app by name and component. Expects the function csr to have been called on the server side.
  * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the server.
  * @param {React.FunctionComponent | React.ComponentClass | string} app - The React component/app to render.
  */
--- server.js
+++ server.js
@@ -4,18 +4,16 @@ import React from "react";
 /**
  * Server renders a React app/component and returns an HTML string ready for insertion into the DOM.
  * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the client.
- * @param {React.FunctionComponent | React.ComponentClass | string} app - The React component/app to render.
- * @param {object} props - Application props. These will be serialised and passed to the client for hydration as well as used to SSR the application/component.
+ * @param {any} app - The React component/app to render.
+ * @param {any} props - Application props. These will be serialised and passed to the client for hydration as well as used to SSR the application/component.
+ * @param {any} [replacer] - A function that transforms the results. This function is called for each member of the object. If a property is excluded then the value returned is null.
  * @returns {string}
  */
-export function ssr(name, app, props) {
-  return `<script>
-  window.__REACT_DATA__ = window.__REACT_DATA__ || new Map();
-  window.__REACT_DATA__.set('${name}', ${JSON.stringify(props)});
-</script>
-<style>${name}:not(:defined) > template[shadowrootmode] ~ *  { display: none; }</style>
+export function ssr(name, app, props, replacer) {
+  return `<style>${name}:not(:defined) > template[shadowrootmode] ~ *  { display: none; }</style>
 <${name}>
   <template shadowrootmode="open">
+    <script type="application/json">${JSON.stringify(props, replacer)}</script>
     <div id="${name}">${ReactDOM.renderToString(React.createElement(app, props))}</div>
   </template>
 </${name}>
