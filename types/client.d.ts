1debf5da0bf9d99ec51244c54d26be5738854c66
Richard Walker <digitalsadhu@gmail.com>
2023-06-22 12:41:39 +1200

    tweak publish setup and uncommit types

--- types/client.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Hydrates an SSR'd React app on the client side by name and component. Expects ssr to be have been called on the server.
- * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the server.
- * @param {any} app - The React component/app to render.
- * @param {{ reviver?: any }} [options] - Options object
- */
-export function ssr(name: string, app: any, { reviver }?: {
-    reviver?: any;
-}): void;
