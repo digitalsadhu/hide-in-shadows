8807f810cffc8da9fefc474b54317493ca057e2e
Richard Walker <digitalsadhu@gmail.com>
2023-06-23 15:28:31 +1200

    fix: improve types

--- client.js
+++ client.js
@@ -2,10 +2,15 @@ import { hydrateRoot } from "react-dom/client";
 import React from "react";
 
 /**
+ * @typedef {Object} HydrateOptions
+ * @property {(this: any, key: string, value: any) => any} [reviver] - A function that transforms the results. This function is called for each member of the object. If a property is excluded then the value returned is null.
+ */
+
+/**
  * Hydrates an SSR'd React app on the client side by name and component. Expects ssr to be have been called on the server.
  * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the server.
  * @param {any} app - The React component/app to render.
- * @param {{ reviver?: any }} [options] - Options object
+ * @param {HydrateOptions} [options] - Options object
  */
 export function ssr(name, app, { reviver } = {}) {
   if (!customElements.get(name)) {
--- server.js
+++ server.js
@@ -3,18 +3,20 @@ import ReactDOM from "react-dom/server";
 import React from "react";
 
 /**
+ * @template TProps
  * @typedef {Object} SSROptions
- * @property {any} [props] - Application props. These will be serialised and passed to the client for hydration as well as used to SSR the application/component.
- * @property {any} [replacer] - A function that transforms the results. This function is called for each member of the object. If a property is excluded then the value returned is null.
+ * @property {TProps} [props] - Application props. These will be serialised and passed to the client for hydration as well as used to SSR the application/component.
+ * @property {(this: any, key: string, value: any) => any} [replacer] - A function that transforms the results. This function is called for each member of the object. If a property is excluded then the value returned is null.
  * @property {string} [styles] - Inline styling to be added to the shadow root.
- * @property {string} [mode] - The mode of the shadow root. Can be "open" or "closed". Defaults to "open".
+ * @property {"open" | "closed"} [mode] - The mode of the shadow root. Can be "open" or "closed". Defaults to "open".
  */
 
 /**
  * Server renders a React app/component and returns an HTML string ready for insertion into the DOM.
+ * @template TProps
  * @param {string} name - The name of the app. Lower case characters and "-" character only. This value will be used as the custom element name. Must match the name used on the client.
  * @param {string | function} app - The React component/app to render. Can be a React component or a pre-rendered string.
- * @param {SSROptions} [options] - Application props. These will be serialised and passed to the client for hydration as well as used to SSR the application/component.
+ * @param {SSROptions<TProps>} [options] - Application props. These will be serialised and passed to the client for hydration as well as used to SSR the application/component.
  * @returns {string}
  */
 export function ssr(name, app, { props, replacer, styles, mode = "open" } = {}) {
