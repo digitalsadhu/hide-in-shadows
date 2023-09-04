12c180a3ef1d98c4d0d4a240bdc57224261f09b9
Richard Walker <digitalsadhu@gmail.com>
2023-09-04 15:26:24 +1200

    fix: use dsd-polyfill module instead of own polyfill in react lib

---
+++ react/rollup.config.js
@@ -0,0 +1,13 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "polyfill.js",
+  output: {
+    file: "dist/polyfill.js",
+    format: "iife",
+    sourcemap: false,
+  },
+  plugins: [resolve(), commonjs(), terser()],
+};
